dc917163a5fdda7e928ed65da2a7eb0228d172a3
Kenn Knowles <kenn.knowles@gmail.com>
2013-04-22 16:44:45 -0400

    Add some Models spec

--- spec/Models.spec.js
+++ spec/Models.spec.js
@@ -17,4 +17,19 @@ define([
     c = ko.computed,
     expect = chai.expect,
     assert = chai.assert;
+
+    describe("Models <: Observable {:Observable Model}" , function() {
+        it('Writes new models to their URI, otherwise sets all their attributes', function() {
+            var m = ss.LocalModel({ attributes: { foo: 'baz', resource_uri: '/fake/uri/1' } });
+            var models = ss.Models({ models: { "/fake/uri/1": m } });
+
+            expect(_(models()).keys()).to.deep.equal(['/fake/uri/1']);
+            expect(models()['/fake/uri/1'].attributes().foo()).to.equal('baz');
+
+            models({ '/fake/uri/1': ss.LocalModel({ attributes: { foo: 'bizzle', resource_uri: '/fake/uri/1'} }) });
+
+            expect(models()['/fake/uri/1'].attributes().foo()).to.equal('bizzle');
+            expect(m.attributes().foo()).to.equal('bizzle');
+        });
+    });
 });
